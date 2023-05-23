c3ad0241ba0e76b3c001cc1d9c9ccf52318fd435
Eesa Abdulraouf <eesaaraouf@gmail.com>
2023-05-23 14:52:43 -0400

    main success and all data graphs created

--- dfClass.py
+++ dfClass.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import datetime
 import os
 
-class dfDay:
+class DataframeDay:
     def __init__(self, grade_level, date, df):
         self.df = df
         self.date = datetime.datetime.strptime(date, '%m-%d-%Y').date()
@@ -17,17 +18,17 @@ class dfDay:
             self.release_time = "3:15"
         elif(grade_level=="MS"):
             self.grade_level = "Middle School"
-            self.release_time = "IDK"
+            self.release_time = "3:15"
         else:
             self.grade_level = "Elementary School"
-            self.release_time = "IDK"
+            self.release_time = "2:00"
         
 
     def clean(self):
         self.df["Time In"] = pd.to_datetime(self.df["Time In"])
         self.df["Time Out"] = pd.to_datetime(self.df["Time Out"])
         self.df["Difference"] = self.df["Time Out"] - self.df["Time In"]
-        for index, row in hs1104df.iterrows():
+        for index, row in self.df.iterrows():
             if (row["Difference"].total_seconds()/60) > 300:
                 self.df.drop(index, inplace=True)
         self.df["Diff Mins"] = self.df["Diff Mins"].astype(int)
@@ -44,16 +45,48 @@ class dfDay:
         plt.ylabel(f"Time spent in school (minutes)",fontsize="30")
         plt.show()
 
+    def scatter_in_timediff(self):
+        '''Plots a scatter plot relating the ACTUAL time of entering from the release time to the Time difference from time in time out.'''
+        fig, ax = plt.subplots()
+        plot = sns.scatterplot(data=self.df,
+                        x="Time In", y="Diff Mins", hue="Grade", palette=self.cmrpalette,s=100)
+        fig.suptitle(f"{self.grade_level} {self.date.strftime(r'%A %B %d, %Y')} ",fontsize="48")
+        plt.legend(fontsize="30")
+        plt.xlabel(f"Time Arrived",fontsize="30")
+        plt.ylabel(f"Time spent in school (minutes)",fontsize="30")
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M'))
+        plt.tick_params(axis='both', which='major', labelsize=24)
+        plt.show()
+
+
     def lmplot_release_timediff(self):
         '''Plots a scatter plot (with LSRL) relating the time difference of entering from the release time to the Time difference from time in time out.'''
         fig, ax = plt.subplots()
         plot = sns.lmplot(data=self.df,
                         x="Diff Release", y="Diff Mins", palette=self.cmrpalette)
-        fig.suptitle(f"{self.grade_level} {self.date.strftime(r'%A %B %d, %Y')} ",fontsize="48")
+        plt.title(f"{self.grade_level} {self.date.strftime(r'%A %B %d, %Y')} ",fontsize="36")
         plt.legend(fontsize="30")
         plt.xlabel(f"Time from release at {self.release_time} (minutes)",fontsize="30")
         plt.ylabel(f"Time spent in school (minutes)",fontsize="30")
         plt.show()
+
+    @staticmethod
+    def oneDayScatter(df1, df2, df3):
+        '''Creates a plot of all the grade levels from one singular day'''
+        DFs = [df1.df,df2.df,df3.df]
+        maindf = pd.concat(DFs)
+        fig, ax = plt.subplots()
+        plot = sns.scatterplot(data=maindf,
+                        x="Time In", y="Diff Mins", hue="Level",s=100)
+        fig.suptitle(f"All Levels {df1.date.strftime(r'%A %B %d, %Y')} ",fontsize="48")
+        plt.legend(fontsize="30")
+        plt.xlabel(f"Time Arrrived",fontsize="30")
+        plt.ylabel(f"Time spent in school (minutes)",fontsize="30")
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%I:%M'))
+        plt.tick_params(axis='both', which='major', labelsize=24)
+        plt.show()
+
+
 
     def showData(self):
         return self.df
@@ -61,9 +94,10 @@ class dfDay:
         return f"{self.date} + {self.grade_level}"
 
 
-absolute_path = os.path.abspath("")
+# absolute_path = os.path.abspath("")
 
-hs1104df = pd.read_csv(absolute_path + "\main\maintestHS1104v2.csv")
-data = dfDay("HS","11-4-2022",hs1104df)
-print(data)
-print(data.lmplot_release_timediff())
+# TESThs1104df = pd.read_csv(absolute_path + "\maintestHS1104v2.csv")
+# data = DataframeDay("HS","11-4-2022",TESThs1104df)
+# print(data)
+# print(data.scatter_in_timediff())
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import dfClass as dfcl
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
+
+absolute_path = os.path.abspath("")
+
+#importing all the data
+hs1104df = pd.read_csv(absolute_path + "\Data\HS_11-04-2022.csv")
+hs1102df = pd.read_csv(absolute_path + "\Data\HS_11-02-2022.csv")
+ms1104df = pd.read_csv(absolute_path + "\Data\MS_11-04-2022.csv")
+ms1102df = pd.read_csv(absolute_path + "\Data\MS_11-02-2022.csv")
+es1104df = pd.read_csv(absolute_path + "\Data\ES_11-04-2022.csv")
+es1102df = pd.read_csv(absolute_path + "\Data\ES_11-02-2022.csv")
+
+# defining all the objects for all entries
+hs1104 = dfcl.DataframeDay("HS", "11-4-2022",hs1104df)
+hs1102 = dfcl.DataframeDay("HS", "11-2-2022",hs1102df)
+
+ms1104 = dfcl.DataframeDay("MS", "11-4-2022",ms1104df)
+ms1102 = dfcl.DataframeDay("MS", "11-2-2022",ms1102df)
+
+es1104 = dfcl.DataframeDay("ES", "11-4-2022",es1104df)
+es1102 = dfcl.DataframeDay("ES", "11-2-2022",es1102df)
+
+es1102.scatter_in_timediff()
